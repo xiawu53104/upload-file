62936cc4bdb47e153781e4f4681bdd706a45f3f1
wu_xia <wu_xia@daydao.com>
2020-01-16 11:44:59 +0800

    update

---
+++ src/pages/upload-file/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="file" @change="handlerFileChange($event)">
+    <el-button>上传</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
+@Component
+export default class UploadFile extends Vue {
+  selectedFile!: File | null;
+
+  handlerFileChange (e: HTMLInputEvent) {
+    this.selectedFile = e.target.files && e.target.files[0]
+    console.log(this.selectedFile)
+  };
+}
+</script>
+
+<style lang="less">
+
+</style>
